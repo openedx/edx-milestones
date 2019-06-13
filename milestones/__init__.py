963f832f800ac9f236c28f3d9e5c7bd201148f70
Jeremy Bowman <jbowman@edx.org>
2019-06-13 14:39:45 -0400

    OEP-18 compliance, dependency upgrades (#38)

--- milestones/__init__.py
+++ milestones/__init__.py
@@ -2,4 +2,5 @@
 Milestones app initialization module
 """
 from __future__ import unicode_literals
-__version__ = '0.2.2'
+
+__version__ = '0.2.3'
--- settings.py
+++ settings.py
@@ -1,11 +1,17 @@
+"""
+These settings are here to use during tests, because django requires them.
+
+In a real-world use case, apps in this project are installed into other
+Django applications, so these settings will not be used.
+"""
 from __future__ import unicode_literals
-DEBUG=True
-TEST_MODE=True
+DEBUG = True
+TEST_MODE = True
 TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 TRANSACTIONS_MANAGED = {}
 USE_TZ = False
 TIME_ZONE = {}
-SECRET_KEY='SHHHHHH'
+SECRET_KEY = 'SHHHHHH'
 
 DATABASES = {
     'default': {
