bde29071225f6b581b05d606468530e9b4b257d5
Salman Nawaz <salman.nawaz@A006-01081.local>
2023-07-11 22:29:54 +0500

    fix: django 4.2 upgrade

--- setup.py
+++ setup.py
@@ -79,6 +79,7 @@ setup(
         'Framework :: Django',
         'Framework :: Django :: 3.2',
         'Framework :: Django :: 4.0',
+        'Framework :: Django :: 4.2',
     ],
     packages=find_packages(exclude=["tests"]),
     install_requires=load_requirements('requirements/base.in'),
